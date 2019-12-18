520d67b55071e6d911d5251dbe96e2c3268195ff
Bruno Garcia <bruno@brunogarcia.com>
2019-12-18 16:41:41 +0100

    bash

--- build.cmd
+++ build.cmd
@@ -1,22 +1,22 @@
-pushd src/SymbolCollector.Android/
-msbuild /restore /p:Configuration=Release \
-    /p:AndroidBuildApplicationPackage=true \
-    /t:Clean\;Build\;SignAndroidPackage
+pushd src\SymbolCollector.Android\
+msbuild /restore /p:Configuration=Release ^
+    /p:AndroidBuildApplicationPackage=true ^
+    /t:Clean;Build;SignAndroidPackage
 popd
 
-pushd src/SymbolCollector.Server/
+pushd src\SymbolCollector.Server\
 dotnet build -c Release
 popd
 
-pushd test/SymbolCollector.Server.Tests/
+pushd test\SymbolCollector.Server.Tests\
 dotnet test -c Release
 popd
 
-pushd test/SymbolCollector.Core.Tests/
+pushd test\SymbolCollector.Core.Tests\
 dotnet test -c Release
 popd
 
-pushd src/SymbolCollector.Console/
+pushd src\SymbolCollector.Console\
 dotnet publish -c release /p:PublishSingleFile=true --self-contained -r osx-x64
 dotnet publish -c release /p:PublishSingleFile=true --self-contained -r linux-x64
 dotnet publish -c release /p:PublishSingleFile=true --self-contained -r linux-musl-x64
