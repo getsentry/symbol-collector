b099e223316d4097232536da80d829b7eaba98b4
Bruno Garcia <bruno@brunogarcia.com>
2019-12-26 17:29:59 +0100

    forms

--- src/SymbolCollector.Xamarin.Android/MainActivity.cs
+++ src/SymbolCollector.Xamarin.Android/MainActivity.cs
@@ -6,51 +6,32 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Util;
 using Java.Lang;
+using Microsoft.Extensions.Logging;
 using SymbolCollector.Core;
+using SymbolCollector.Xamarin.Forms;
 using Exception = System.Exception;
 
 namespace SymbolCollector.Xamarin.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         private const string Tag = "MainActivity";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-            _ = StartUpload();
-        }
 
-        private Task StartUpload()
-        {
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             var bundle = PackageManager.GetApplicationInfo(PackageName, global::Android.Content.PM.PackageInfoFlags.MetaData).MetaData;
             var url = bundle.GetString("io.sentry.symbol-collector");
+
             Log.Info(Tag, "Using Symbol Collector endpoint: " + url);
 
-            return Task.Run(async () =>
-            {
-                var paths = new[] {
-                    "/system/lib",
-                    "/system/lib64",
-                    "/system/"};
+            LoadApplication(new App(url));
 
-                var client = new Client(
-                    new Uri(url),
-                    new ObjectFileParser(logger: new LoggerAdapter<ObjectFileParser>()),
-                    assemblyName: GetType().Assembly.GetName(),
-                    logger: new LoggerAdapter<Client>());
-                try
-                {
-                    await client.UploadAllPathsAsync(paths, CancellationToken.None);
-                }
-                catch (Exception e)
-                {
-                    Log.Error(Tag, Throwable.FromException(e), "Failed uploading.");
-                }
-            });
+            // // Set our view from the "main" layout resource
+            // SetContentView(Resource.Layout.activity_main);
         }
     }
 }
---
+++ src/SymbolCollector.Xamarin.Forms/App.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SymbolCollector.Core;
+using Exception = System.Exception;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace SymbolCollector.Xamarin.Forms
+{
+    public partial class App
+    {
+        private readonly string _serverEndpoint;
+
+        public App(string serverEndpoint)
+        {
+            _serverEndpoint = serverEndpoint ?? throw new ArgumentNullException(nameof(serverEndpoint));
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            _ = StartUpload(_serverEndpoint);
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        private Task StartUpload(string url)
+        {
+
+            return Task.Run(async () =>
+            {
+                var paths = new[] {
+                    "/system/lib",
+                    "/system/lib64",
+                    "/system/"};
+
+                var client = new Client(
+                    new Uri(url),
+                    new ObjectFileParser(
+                        // TODO: logging
+                    ),
+                    assemblyName: GetType().Assembly.GetName());
+                    // logger: new LoggerAdapter<Client>());
+                try
+                {
+                    await client.UploadAllPathsAsync(paths, CancellationToken.None);
+                }
+                catch (Exception e)
+                {
+                    // TODO logging
+                    Console.WriteLine(e);
+                    // Log.Error(Tag, Throwable.FromException(e), "Failed uploading.");
+                }
+            });
+        }
+    }
+}
