2c233eac73ae2b0b249167ec18ff041c95303504
Bruno Garcia <bruno@brunogarcia.com>
2020-01-17 17:07:51 +0100

    release 1.0.2

--- src/SymbolCollector.Android/Properties/AssemblyInfo.cs
+++ src/SymbolCollector.Android/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SymbolCollector.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("sentry.io")]
 [assembly: AssemblyProduct("SymbolCollector.Android")]
-[assembly: AssemblyCopyright("Copyright Sentry ©  2019")]
+[assembly: AssemblyCopyright("Copyright Sentry © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
