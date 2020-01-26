3e38ac2976f79a780e7a3ae4041169da172822ab
Bruno Garcia <bruno@brunogarcia.com>
2020-01-26 12:47:27 -0500

    bump android version on release

--- src/SymbolCollector.Android/Properties/AssemblyInfo.cs
+++ src/SymbolCollector.Android/Properties/AssemblyInfo.cs
@@ -14,15 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Until Xamarin supports SDK style csproj
 [assembly: AssemblyVersion("1.0.2.0")]
 [assembly: AssemblyFileVersion("1.0.2.0")]
