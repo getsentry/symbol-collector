65a6f2951fc62bb5abaa93c90f8c2975f94f3314
Bruno Garcia <bruno@brunogarcia.com>
2020-01-26 09:49:28 -0500

    test bundle id

--- src/SymbolCollector.Core/BundleIdGenerator.cs
+++ src/SymbolCollector.Core/BundleIdGenerator.cs
@@ -1,22 +1,26 @@
-using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SymbolCollector.Core
 {
     public class BundleIdGenerator
     {
         private readonly SuffixGenerator _suffixGenerator;
+        private readonly Regex _removeCharsRegex;
 
-        public BundleIdGenerator(SuffixGenerator suffixGenerator) => _suffixGenerator = suffixGenerator;
-
-        public string CreateBundleId(string friendlyName)
+        public BundleIdGenerator(SuffixGenerator suffixGenerator)
         {
-            var invalids = Path.GetInvalidFileNameChars().Concat(" ").ToArray();
-            return string.Join("_",
-                    friendlyName.Split(invalids, StringSplitOptions.RemoveEmptyEntries)
-                        .Append(_suffixGenerator.Generate()))
-                .TrimEnd('.');
+            var invalidChars = Path.GetInvalidFileNameChars()
+                .Concat(new[] {' ', '*', '?', '"', '\n'});
+            var pattern = Regex.Escape(new string(invalidChars.ToArray()));
+            _removeCharsRegex = new Regex($"[{pattern}]", RegexOptions.Compiled);
+            _suffixGenerator = suffixGenerator;
         }
+
+        public string CreateBundleId(string friendlyName) =>
+            _removeCharsRegex
+                .Replace(friendlyName, "_")
+                .Trim('.') + _suffixGenerator.Generate();
     }
 }
