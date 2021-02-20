0dd644d0d67336217dada5da81c9e1b5fa0aabed
Bruno Garcia <bruno@brunogarcia.com>
2021-02-20 16:11:27 -0500

    release: 1.2.2

--- src/SymbolCollector.Android/Properties/AssemblyInfo.cs
+++ src/SymbolCollector.Android/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Until Xamarin supports SDK style csproj
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
