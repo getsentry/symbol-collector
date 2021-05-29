a516320074856caad43ba141da7c30b12520a177
Bruno Garcia <bruno@brunogarcia.com>
2021-05-29 11:59:11 -0400

    run local

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,6 @@
 export apk=src/SymbolCollector.Android/bin/Debug/net6.0-android/io.sentry.symbolcollector.android-Signed.apk
 rm $apk
 dotnet build src/SymbolCollector.Android
+adb uninstall io.sentry.symbolcollector.android
 adb install -r $apk
+adb shell am start -n io.sentry.symbolcollector.android/io.sentry.symbolcollector.MainActivity
