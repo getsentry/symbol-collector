32db2dedfcf7bef25701c7408436b875ea51047b
Bruno Garcia <bruno@brunogarcia.com>
2022-10-07 15:56:44 -0700

    bump dotnet - dyn sampl support

--- run.sh
+++ run.sh
@@ -6,18 +6,18 @@ set -e
 
 export appcenter_batch_count=${appcenter_batch_count:-1}
 export apk_path=src/SymbolCollector.Android/bin/Release/net6.0-android/publish/io.sentry.symbolcollector.android-Signed.apk
-rm $apk_path && echo deleted apk || echo apk not there
+# rm $apk_path && echo deleted apk || echo apk not there
 
 # Always a source of issues trying to find the Android SDK:
 # \ Preparing tests... Object reference not set to an instance of an object
-rm ~/.config/xbuild/monodroid-config.xml || echo monodroid config didnt exist
+# rm ~/.config/xbuild/monodroid-config.xml || echo monodroid config didnt exist
 
 pushd src/SymbolCollector.Android/
-dotnet publish -c Release
+# dotnet publish -c Release
 popd
 
 pushd test/SymbolCollector.Android.UITests/
-msbuild /restore /p:Configuration=Release /t:Build
+# msbuild /restore /p:Configuration=Release /t:Build
 
 pushd bin/Release/
 for barch_number in $(seq 1 1 $appcenter_batch_count); do
@@ -29,5 +29,5 @@ for barch_number in $(seq 1 1 $appcenter_batch_count); do
         --locale "en_US" \
         --build-dir . \
         --async \
-        --uitest-tools-dir ~/.nuget/packages/xamarin.uitest/3.2.8/tools/
+        --uitest-tools-dir ~/.nuget/packages/xamarin.uitest/3.2.8/tools/ || true
 done
--- src/SymbolCollector.Android/MainActivity.cs
+++ src/SymbolCollector.Android/MainActivity.cs
@@ -22,6 +22,7 @@ namespace SymbolCollector.Android
 {
     [Activity(
         Name = "io.sentry.symbolcollector.MainActivity",
+<<<<<<< Updated upstream
         Label = "@string/app_name", MainLauncher = true,
         ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
@@ -320,6 +321,288 @@ namespace SymbolCollector.Android
         {
             base.Dispose(disposing);
             _host.Dispose();
+=======
+        Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true
+        //, ScreenOrientation = ScreenOrientation.Portrait
+    )]
+    public class MainActivity : AppCompatActivity
+    {
+        // private readonly string _friendlyName;
+        // private readonly IHost _host;
+        // private readonly IServiceProvider _serviceProvider;
+        // private readonly ITransaction _startupTransaction;
+
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            // var span = _startupTransaction.StartChild("OnCreate");
+            // try
+            // {
+                base.OnCreate(savedInstanceState);
+                // Platform.Init(this, savedInstanceState);
+
+                SetContentView(Resource.Layout.activity_main);
+
+            //     var footerText = (TextView)base.FindViewById(Resource.Id.footer)!;
+            //     var versionName = Application.Context.ApplicationContext?.PackageManager?
+            //         .GetPackageInfo(Application.Context.ApplicationContext?.PackageName ?? "", 0)?.VersionName;
+            //     footerText.Text = $"Version: {versionName}\n" + footerText.Text;
+            //
+            //     var uploader = _serviceProvider.GetRequiredService<AndroidUploader>();
+            //     var metrics = _serviceProvider.GetRequiredService<ClientMetrics>();
+            //     var uploadButton = (Button)base.FindViewById(Resource.Id.btnUpload)!;
+            //     var cancelButton = (Button)base.FindViewById(Resource.Id.btnCancel)!;
+            //     var url = (EditText)base.FindViewById(Resource.Id.server_url)!;
+            //     var source = new CancellationTokenSource();
+            //
+            //     url.FocusChange += (sender, args) =>
+            //     {
+            //         if (!args.HasFocus)
+            //         {
+            //             SentrySdk.AddBreadcrumb("Unfocus", category: "ui.event");
+            //             Unfocus();
+            //         }
+            //     };
+            //
+            //     uploadButton.Click += OnUploadButtonOnClick;
+            //     cancelButton.Click += OnCancelButtonOnClick;
+            //
+            //     async void OnUploadButtonOnClick(object? sender, EventArgs args)
+            //     {
+            //         SentrySdk.AddBreadcrumb("OnUploadButtonOnClick", category: "ui.event");
+            //         var options = _serviceProvider.GetRequiredService<SymbolClientOptions>();
+            //         options.BaseAddress = new Uri(url.Text!); // TODO validate
+            //
+            //         SentrySdk.ConfigureScope(s => s.SetTag("server-endpoint", options.BaseAddress.AbsoluteUri));
+            //
+            //         Unfocus();
+            //
+            //         uploadButton.Enabled = false;
+            //         source = new CancellationTokenSource();
+            //
+            //         var uploadTask = uploader.StartUpload(_friendlyName, source.Token);
+            //         var updateUiTask = StartUiUpdater(source.Token, metrics);
+            //
+            //         await UploadAsync(uploadTask, updateUiTask, metrics, cancelButton, uploadButton, source);
+            //     }
+            //
+            //     void OnCancelButtonOnClick(object? sender, EventArgs args)
+            //     {
+            //         SentrySdk.AddBreadcrumb("OnCancelButtonOnClick", category: "ui.event");
+            //         Unfocus();
+            //         source.Cancel();
+            //     }
+            //
+            //     span.Finish();
+            //     _startupTransaction.Finish();
+            // }
+            // catch
+            // {
+            //     // TODO: How do I pass the exception so it can connect span to error event later?
+            //     span.Finish(SpanStatus.InternalError);
+            //     _startupTransaction.Finish(SpanStatus.InternalError);
+            //     throw;
+            // }
+>>>>>>> Stashed changes
         }
+//
+//         private void Unfocus()
+//         {
+//             if (CurrentFocus?.WindowToken is { } windowToken)
+//             {
+//                 (GetSystemService(InputMethodService) as InputMethodManager)
+//                     ?.HideSoftInputFromWindow(windowToken, 0);
+//             }
+//         }
+//
+//         private async Task UploadAsync(
+//             Task uploadTask,
+//             Task updateUiTask,
+//             ClientMetrics metrics,
+//             View cancelButton,
+//             View uploadButton,
+//             CancellationTokenSource source)
+//         {
+//             var container = base.FindViewById(Resource.Id.metrics_container)!;
+//             container.Visibility = ViewStates.Visible;
+//
+//             var doneText = (TextView)base.FindViewById(Resource.Id.done_text)!;
+//             var ranForLabel = (TextView)base.FindViewById(Resource.Id.ran_for_label)!;
+//             var ranForContainer = base.FindViewById(Resource.Id.ran_for_container)!;
+//             var ranForView = base.FindViewById(Resource.Id.ran_for_view)!;
+//
+//             try
+//             {
+//                 cancelButton.Enabled = true;
+//                 await Task.WhenAny(uploadTask, updateUiTask);
+//                 if (uploadTask.IsCompletedSuccessfully)
+//                 {
+//                     cancelButton.Enabled = false;
+//                     uploadButton.Enabled = false;
+//
+//                     doneText.Visibility = ViewStates.Visible;
+//                     ranForView.Visibility = ViewStates.Visible;
+//                     ranForContainer.Visibility = ViewStates.Visible;
+//
+//                     ranForLabel.Text = metrics.RanFor.ToString();
+//                 }
+//                 else if (uploadTask.IsFaulted)
+//                 {
+//                     ShowError(uploadTask.Exception);
+//                 }
+//                 else
+//                 {
+//                     cancelButton.Enabled = false;
+//                     uploadButton.Enabled = true;
+//                 }
+//             }
+//             catch (Exception e)
+//             {
+//                 ShowError(e);
+//             }
+//             finally
+//             {
+//                 source.Cancel();
+//             }
+//         }
+//
+//         private Task StartUiUpdater(CancellationToken token, ClientMetrics metrics) =>
+//             Task.Run(async () =>
+//             {
+//                 var uploadedCount = (TextView)base.FindViewById(Resource.Id.uploaded_count)!;
+//                 var startedTime = (TextView)base.FindViewById(Resource.Id.started_time)!;
+//                 var alreadyExisted = (TextView)base.FindViewById(Resource.Id.already_existed)!;
+//                 var filesProcessed = (TextView)base.FindViewById(Resource.Id.files_processed)!;
+//                 var successfullyUpload = (TextView)base.FindViewById(Resource.Id.successfully_upload)!;
+//                 var elfFiles = (TextView)base.FindViewById(Resource.Id.elf_files)!;
+//                 var failedParsing = (TextView)base.FindViewById(Resource.Id.failed_parsing)!;
+//                 var failedUploading = (TextView)base.FindViewById(Resource.Id.failed_uploading)!;
+//                 var jobsInFlight = (TextView)base.FindViewById(Resource.Id.jobs_in_flight)!;
+//                 var directoryNotFound = (TextView)base.FindViewById(Resource.Id.directory_not_found)!;
+//                 var fileNotFound = (TextView)base.FindViewById(Resource.Id.file_not_found)!;
+//                 var unauthorizedAccess = (TextView)base.FindViewById(Resource.Id.unauthorized_access)!;
+//
+//                 while (!token.IsCancellationRequested)
+//                 {
+//                     RunOnUiThread(() =>
+//                     {
+//                         uploadedCount.Text = metrics.UploadedBytesCountHumanReadable();
+//                         startedTime.Text = metrics.StartedTime.ToString();
+//                         alreadyExisted.Text = metrics.AlreadyExistedCount.ToString();
+//                         filesProcessed.Text = metrics.FilesProcessedCount.ToString();
+//                         successfullyUpload.Text = metrics.SuccessfullyUploadCount.ToString();
+//                         elfFiles.Text = metrics.ElfFileFoundCount.ToString();
+//                         failedParsing.Text = metrics.FailedToParseCount.ToString();
+//                         failedUploading.Text = metrics.FailedToUploadCount.ToString();
+//                         jobsInFlight.Text = metrics.JobsInFlightCount.ToString();
+//                         directoryNotFound.Text = metrics.DirectoryDoesNotExistCount.ToString();
+//                         fileNotFound.Text = metrics.FileDoesNotExistCount.ToString();
+//                         unauthorizedAccess.Text = metrics.FileOrDirectoryUnauthorizedAccessCount.ToString();
+//                     });
+//                     try
+//                     {
+//                         await Task.Delay(250, token);
+//                     }
+//                     catch (OperationCanceledException)
+//                     {
+//                     }
+//                 }
+//             }, token);
+//
+//         private void ShowError(Exception? e)
+//         {
+//             if (e is null)
+//             {
+//                 SentrySdk.CaptureMessage("ShowError called but no Exception instance provided.", SentryLevel.Error);
+//             }
+//
+//             if (e is AggregateException ae && ae.InnerExceptions.Count == 1)
+//             {
+//                 e = ae.InnerExceptions[0];
+//             }
+//
+//             var uploadButton = (Button)base.FindViewById(Resource.Id.btnUpload)!;
+//             var cancelButton = (Button)base.FindViewById(Resource.Id.btnCancel)!;
+//
+//             var lastEvent = SentrySdk.LastEventId;
+//             // TODO: SentryId.Empty should operator overload ==
+//             var message = SentryId.Empty.ToString() == lastEvent.ToString()
+//                 ? e?.ToString() ?? "Something didn't quite work."
+//                 : $"Sentry id {lastEvent}:\n{e}";
+//
+//             var builder = new AlertDialog.Builder(this)
+//                 .SetTitle("Error")
+//                 ?.SetMessage(message)
+//                 ?.SetNeutralButton("Ironic eh?", (o, eventArgs) =>
+//                 {
+//                     uploadButton.Enabled = true;
+//                     cancelButton.Enabled = false;
+//                 });
+//             if (builder is null)
+//             {
+//                 throw new InvalidOperationException("Couldn't get a dialog built.");
+//             }
+//
+//             builder.Show();
+//         }
+//
+//         public MainActivity()
+//         {
+// #pragma warning disable 618
+//             _friendlyName = $"Android:{Build.Manufacturer}-{Build.CpuAbi}-{Build.Model}";
+// #pragma warning restore 618
+//             _host = Host.Init();
+//             _serviceProvider = _host.Services;
+//
+//             var tran = SentrySdk.StartTransaction("AppStart", "activity.load");
+//             _startupTransaction = tran;
+//
+//             StructUtsname? uname = null;
+//             try
+//             {
+//                 uname = Os.Uname();
+//                 _friendlyName += $"-kernel-{uname?.Release ?? "??"}";
+//             }
+//             catch (Exception e)
+//             {
+//                 SentrySdk.AddBreadcrumb("Couldn't run uname", category: "exec",
+//                     data: new Dictionary<string, string> {{"exception", e.Message}}, level: BreadcrumbLevel.Error);
+//                 // android.runtime.JavaProxyThrowable: System.NotSupportedException: Could not activate JNI Handle 0x7ed00025 (key_handle 0x4192edf8) of Java type 'md5eb7159ad9d3514ee216d1abd14b6d16a/MainActivity' as managed type 'SymbolCollector.Android.MainActivity'. --->
+//                 // Java.Lang.NoClassDefFoundError: android/system/Os ---> Java.Lang.ClassNotFoundException: Didn't find class "android.system.Os" on path: DexPathList[[zip file "/data/app/SymbolCollector.Android.SymbolCollector.Android-1.apk"],nativeLibraryDirectories=[/data/app-lib/SymbolCollector.Android.SymbolCollector.Android-1, /vendor/lib, /system/lib]]
+//             }
+//
+//             SentrySdk.ConfigureScope(s =>
+//             {
+//                 s.Transaction = tran;
+//                 s.SetTag("friendly-name", _friendlyName);
+//
+//                 if (uname is { })
+//                 {
+//                     s.Contexts["uname"] = new
+//                     {
+//                         uname.Machine,
+//                         uname.Nodename,
+//                         uname.Release,
+//                         uname.Sysname,
+//                         uname.Version
+//                     };
+//                     s.Contexts.OperatingSystem.KernelVersion = uname.Release;
+//                 }
+// #if DEBUG
+//                 s.SetTag("build-type", "debug");
+//                 // It's 'production' by default otherwise
+//                 s.Environment = "development";
+// #elif RELEASE
+//                 s.SetTag("build-type", "release");
+// #else
+//                 s.SetTag("build-type", "other");
+// #endif
+//             });
+//         }
+//
+//         protected override void Dispose(bool disposing)
+//         {
+//             base.Dispose(disposing);
+//             _host.Dispose();
+//         }
     }
 }
