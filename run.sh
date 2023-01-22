ec9c41a97a25b54c8793ec95aa9d982a46b1bd71
Bruno Garcia <bruno@brunogarcia.com>
2023-01-22 12:38:17 -0500

    runs

--- run.sh
+++ run.sh
@@ -5,22 +5,19 @@ set -e
 # Builds the app and the UI tests in release mode and schedule it on appcenter
 
 export appcenter_batch_count=${appcenter_batch_count:-1}
-#export apk_path=src/SymbolCollector.Android/uitest/io.sentry.symbolcollector.android-Signed.apk
-export apk_path=src/SymbolCollector.Android/bin/Release/net7.0-android/publish/io.sentry.symbolcollector.android-Signed.apk
-#rm $apk_path && echo deleted apk || echo apk not there
+export apk_path=src/SymbolCollector.Android/uitest/io.sentry.symbolcollector.android-Signed.apk
+rm $apk_path && echo deleted apk || echo apk not there
 
 # Always a source of issues trying to find the Android SDK:
 # \ Preparing tests... Object reference not set to an instance of an object
-#rm ~/.config/xbuild/monodroid-config.xml || echo monodroid config didnt exist
+rm ~/.config/xbuild/monodroid-config.xml || echo monodroid config didnt exist
 
 pushd src/SymbolCollector.Android/
-#dotnet publish -c Release -o uitest
-dotnet publish -c Release
+dotnet publish -c Release -o uitest
 popd
 
 pushd test/SymbolCollector.Android.UITests/
-#dotnet build -c Release -o uitest
-msbuild /restore /p:Configuration=Release /t:Build
+dotnet build -c Release -o uitest
 
 pushd uitest/
 cp ../../../$apk_path .
@@ -28,12 +25,10 @@ for barch_number in $(seq 1 1 $appcenter_batch_count); do
 	echo Running Batch \#$barch_number
     appcenter test run uitest --app $SC_APP \
         --devices $SC_DEVICE_SET \
-        --app-path  ../../../$apk_path \
+        --app-path  *.apk \
         --test-series "master" \
         --locale "en_US" \
         --build-dir . \
         --async \
-        --debug \
-        --uitest-tools-dir ~/.nuget/packages/xamarin.uitest/3.2.8/tools/
-#        --uitest-tools-dir .
+        --uitest-tools-dir .
 done
