09575427edac99d9f7862cc012d87a50b7b70d2b
Bruno Garcia <bruno@brunogarcia.com>
2023-08-22 21:10:15 -0400

    use apk from github release (#157)

--- run.sh
+++ run.sh
@@ -1,31 +1,33 @@
 set -e
 
+export version=1.11.1
+export apk=io.sentry.symbolcollector.android-Signed.apk
+export localApk=apk/$version/$apk
+
 # Requirements: appcenter CLI. Env vars: SC_DEVICE_SET and SC_APP
 # Optionally appcenter_batch_count to define how many batches to run. Default is 1.
 # Builds the app and the UI tests in release mode and schedule it on appcenter
 
 export appcenter_batch_count=${appcenter_batch_count:-1}
-export apk_path=src/SymbolCollector.Android/uitest/io.sentry.symbolcollector.android-Signed.apk
-rm $apk_path && echo deleted apk || echo apk not there
 
 # Always a source of issues trying to find the Android SDK:
 # \ Preparing tests... Object reference not set to an instance of an object
 rm ~/.config/xbuild/monodroid-config.xml || echo monodroid config didnt exist
 
-pushd src/SymbolCollector.Android/
-dotnet publish -c Release -o uitest
-popd
-
 pushd test/SymbolCollector.Android.UITests/
 dotnet build -c Release -o uitest
 
 pushd uitest/
-cp ../../../$apk_path .
+
+if [ ! -f "$localApk" ]; then
+  curl -L0 https://github.com/getsentry/symbol-collector/releases/download/$version/$apk --create-dirs -o $localApk
+fi
+
 for barch_number in $(seq 1 1 $appcenter_batch_count); do
 	echo Running Batch \#$barch_number
     appcenter test run uitest --app $SC_APP \
         --devices $SC_DEVICE_SET \
-        --app-path  *.apk \
+        --app-path  $localApk \
         --test-series "master" \
         --locale "en_US" \
         --build-dir . \
--- scripts/bump-version.ps1
+++ scripts/bump-version.ps1
@@ -1,6 +1,6 @@
 param([string] $newVersion)
 
-$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False) 
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
 function Replace-TextInFile {
     param([string] $filePath, [string] $pattern, [string] $replacement)
 
@@ -11,4 +11,5 @@ function Replace-TextInFile {
 
 # Version of .NET assemblies:
 Replace-TextInFile "$PSScriptRoot/../Directory.Build.props" '(?<=<Version>)(.*?)(?=</Version>)' $newVersion
+Replace-TextInFile "$PSScriptRoot/../run.sh" '(?<=version=)(.*)(?=)' $newVersion
 Replace-TextInFile "$PSScriptRoot/../src/SymbolCollector.Android/AndroidManifest.xml" '(?<=versionName=\")(.*)(?=\")' $newVersion
