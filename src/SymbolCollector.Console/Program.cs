01be2bccdbd71ad4519192b38c39f7653cdc72ea
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-01-14 15:26:03 +1300

    Refactored PollyExtensions.SentryPolicy to ResilienceHelpers.SentryRetryStrategy

--- src/SymbolCollector.Console/Program.cs
+++ src/SymbolCollector.Console/Program.cs
@@ -50,37 +50,7 @@ internal class Program
 
                 s.AddHttpClient<ISymbolClient, SymbolClient>()
                     .AddResilienceHandler("retry", builder =>
-                    {
-                        builder.AddRetry(new HttpRetryStrategyOptions
-                        {
-                            BackoffType = DelayBackoffType.Exponential,
-#if RELEASE
-                            // TODO: Until a proper re-entrancy is built in the clients, let it retry for a while
-                            MaxRetryAttempts = 6,
-#else
-                            MaxRetryAttempts = 3,
-#endif
-                            OnRetry = arguments =>
-                            {
-                                var data = new Dictionary<string, string>
-                                {
-                                    { "RetryDelay", arguments.RetryDelay.ToString() },
-                                    { "AttemptNumber", arguments.AttemptNumber.ToString() },
-                                    { "Duration", arguments.Duration.ToString() },
-                                    { "ThreadId", Thread.CurrentThread.ManagedThreadId.ToString() }
-                                };
-                                if (arguments.Outcome.Exception is { } e)
-                                {
-                                    data.Add("exception", e.ToString());
-                                }
-
-                                SentrySdk.AddBreadcrumb(
-                                    $"Waiting {arguments.RetryDelay} following attempt {arguments.AttemptNumber} failed HTTP request.",
-                                    data: data);
-                                return ValueTask.CompletedTask;
-                            }
-                        });
-                    });
+                        builder.AddRetry(ResilienceHelpers.SentryRetryStrategy()));
 
                 s.AddSingleton(Metrics);
                 s.AddSingleton<ConsoleUploader>();
---
+++ src/SymbolCollector.Core/ResilienceHelpers.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
+using Sentry;
+
+namespace SymbolCollector.Core;
+
+public static class ResilienceHelpers
+{
+    public static HttpRetryStrategyOptions SentryRetryStrategy() =>
+        new HttpRetryStrategyOptions
+        {
+            BackoffType = DelayBackoffType.Exponential,
+#if RELEASE
+                            // TODO: Until a proper re-entrancy is built in the clients, let it retry for a while
+                            MaxRetryAttempts = 6,
+#else
+            MaxRetryAttempts = 3,
+#endif
+            OnRetry = arguments =>
+            {
+                var data = new Dictionary<string, string>
+                {
+                    { "RetryDelay", arguments.RetryDelay.ToString() },
+                    { "AttemptNumber", arguments.AttemptNumber.ToString() },
+                    { "Duration", arguments.Duration.ToString() },
+                    { "ThreadId", Thread.CurrentThread.ManagedThreadId.ToString() }
+                };
+                if (arguments.Outcome.Exception is { } e)
+                {
+                    data.Add("exception", e.ToString());
+                }
+
+                SentrySdk.AddBreadcrumb(
+                    $"Waiting {arguments.RetryDelay} following attempt {arguments.AttemptNumber} failed HTTP request.",
+                    data: data);
+                return ValueTask.CompletedTask;
+            }
+        };
+}
