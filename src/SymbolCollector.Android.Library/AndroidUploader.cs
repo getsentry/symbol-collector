e98beaecaee0d0ff11b4e88f0a5fba17aaef9d07
Bruno Garcia <bruno@brunogarcia.com>
2025-06-07 20:58:50 -0400

    fix tracing client-server (#229)
    * fix tracing client-server
    
    * parent-child http trace fixed
    
    * Update src/SymbolCollector.Core/Client.cs
    
    * Update src/SymbolCollector.Core/Client.cs
    
    * Update src/SymbolCollector.Core/Client.cs

--- src/SymbolCollector.Android.Library/AndroidUploader.cs
+++ src/SymbolCollector.Android.Library/AndroidUploader.cs
@@ -15,7 +15,7 @@ public class AndroidUploader
         _logger = logger;
     }
 
-    public Task StartUpload(string friendlyName, CancellationToken token) =>
+    public Task StartUpload(string friendlyName, ISpan uploadSpan, CancellationToken token) =>
         Task.Run(async () =>
         {
             var paths = new[] { "/system/lib", "/system/lib64", "/system/", "/vendor/lib" };
@@ -23,17 +23,21 @@ public class AndroidUploader
             _logger.LogInformation("Using friendly name: {friendlyName} and paths: {paths}",
                 friendlyName, paths);
 
+            var androidUploaderSpan = uploadSpan.StartChild("androidUpload", "uploading all paths async");
             try
             {
-                await _client.UploadAllPathsAsync(friendlyName, BatchType.Android, paths, token);
+                await _client.UploadAllPathsAsync(friendlyName, BatchType.Android, paths, androidUploaderSpan, token);
+                androidUploaderSpan.Finish();
             }
             catch (OperationCanceledException)
             {
+                androidUploaderSpan.Finish(SpanStatus.Cancelled);
             }
             catch (Exception e)
             {
                 _logger.LogError(e, "Failed uploading {friendlyName} paths: {paths}",
                     friendlyName, paths);
+                androidUploaderSpan.Finish(e);
                 // Make sure event is flushed and rethrow
                 await SentrySdk.FlushAsync(TimeSpan.FromSeconds(3));
                 throw;
